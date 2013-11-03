cec9d8840922cfbfee3f087ba1c0948c35adefbf
Shodhan Shah <shodhan2001@new-host.home>
2013-11-03 00:21:21 -0400

    commit to local repository

---
+++ GitTest/SSAddTest.h
@@ -0,0 +1,13 @@
+//
+//  SSAddTest.h
+//  GitTest
+//
+//  Created by Shodhan Shah on 11/3/13.
+//  Copyright (c) 2013 Shodhan Shah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSAddTest : NSObject
+
+@end
---
+++ GitTest/SSAddTest.m
@@ -0,0 +1,13 @@
+//
+//  SSAddTest.m
+//  GitTest
+//
+//  Created by Shodhan Shah on 11/3/13.
+//  Copyright (c) 2013 Shodhan Shah. All rights reserved.
+//
+
+#import "SSAddTest.h"
+
+@implementation SSAddTest
+
+@end
